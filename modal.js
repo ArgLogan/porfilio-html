a1a7060bb9469ccae57c22258eba12a6957891b3
JJROMA <jj_roma@yahoo.com.ar>
2022-02-05 21:36:44 -0300

    004

--- modal.js
+++ modal.js
@@ -1,20 +1,16 @@
-let cerrar = document.getElementById("close");
+let cerrar = document.querySelector("close");
 const abrir = document.querySelector('.cta');
 let modal = document.querySelector(".modal");
 let modalC = document.querySelector(".modal-container");
-console.log(cerrar);
-console.log(document.querySelector(".cta"));
-console.log(modal);
-console.log(modalC);
-console.log("hola perro");
 
-/*abrir.addEventListener("click", function(e){
+
+abrir.addEventListener("click", function(e){
     e.preventDefault();
     modalC.style.opacity = "1";
     modalC.style.visibility="visible";
     modal.classList.toggle("modal-close");
-});*/
+});
 
-/*cerrar.addEventListener("click", function(){
+cerrar.addEventListener("click", function(){
     modal.classList.toggle("modal-close");
-});*/
+});
